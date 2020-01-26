5379d2d4219ce1d6c5a55d605b935106c7a4c7be
Dominique Boutigny <boutigny@in2p3.fr>
2020-01-26 18:29:36 +0100

    add attributes

--- python/human.py
+++ python/human.py
@@ -1,10 +1,21 @@
+from itertools import count
+
 class Human:
-	def __init__(self, gender='female', age=0):
+	_ids = count(0)
+	def __init__(self, gender='female', age=0, city=''):
 		if gender not in ('male', 'female'):
 			raise Exception('Invalid gender - should be male or female')
-		self.gender = gender
-		self.age = age
-		self.status = 'alive'
+		self._gender = gender
+		self._age = age
+		self._status = 'alive'
+		self._city = city
+		self._id = next(self._ids)
 
 	def getAttributes(self):
-		return self.gender, self.age, self.status
+		return self._id, self._gender, self._age, self._status, self._city
+
+	def kill(self):
+		self._status = 'dead'
+
+	def moveTo(self, city):
+		self._city = city
